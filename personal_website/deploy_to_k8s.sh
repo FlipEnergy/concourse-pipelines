5c598a1d99da2e0af86438771034884187af33a5
Dennis Zhang <dennis.zhang.nrg@gmail.com>
2020-09-02 09:51:03 -0700

    rename resources

--- personal_website/deploy_to_k8s.sh
+++ personal_website/deploy_to_k8s.sh
@@ -1,15 +1,15 @@
 #!/bin/sh -e
 
-export DENNIS_SITE_VERSION=`cat personal-website-repo-version/version`
+export DENNIS_SITE_VERSION=`cat personal-website-version-update/version`
 
 echo "Importing secret key..."
 gpg --import concourse-pipelines-repo/personal_website/secret.key
 
 echo "Decrypting kubectl config..."
 mkdir -p ~/.kube
-sops -d personal-website-repo-version/kube_config.enc.yml > ~/.kube/config
+sops -d personal-website-version-update/kube_config.enc.yml > ~/.kube/config
 
-cd personal-website-repo-version
+cd personal-website-version-update
 echo
 
 helmsman --apply -f dennis-site-DSF.yaml
