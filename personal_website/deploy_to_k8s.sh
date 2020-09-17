b893d8d7eac42a0daecb3bf290c05beb99f2d8d9
Dennis Zhang <dennis.zhang.nrg@gmail.com>
2020-09-16 22:39:00 -0700

    use semver attempt

--- personal_website/deploy_to_k8s.sh
+++ personal_website/deploy_to_k8s.sh
@@ -1,15 +1,15 @@
 #!/bin/sh -e
 
-export DENNIS_SITE_VERSION=`cat personal-website-version-update/version`
+export DENNIS_SITE_VERSION=`cat personal-website-repo/version`
 
 echo "Importing secret key..."
 gpg --import concourse-pipelines-repo/common/secret.key
 
 echo "Decrypting kubectl config..."
 mkdir -p ~/.kube
-sops -d personal-website-version-update/kube_config.enc.yml > ~/.kube/config
+sops -d personal-website-repo/kube_config.enc.yml > ~/.kube/config
 
-cd personal-website-version-update
+cd personal-website-repo
 echo
 
 helmsman --apply -f dennis-site-DSF.yaml
