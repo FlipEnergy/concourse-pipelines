ff655b06ef98b93ed4f37a70e1ceeb01724966bc
Dennis Zhang <dennis.zhang.nrg@gmail.com>
2020-11-03 20:37:43 -0800

    fix input

--- personal_website/deploy_to_k8s.sh
+++ personal_website/deploy_to_k8s.sh
@@ -1,16 +1,16 @@
 #!/bin/sh -e
 
-export DENNIS_SITE_VERSION=`cat personal-website-repo/version`
+export DENNIS_SITE_VERSION=`cat personal-website-repo-version-bump/version`
 
 echo "Importing secret key..."
 gpg --import concourse-pipelines-repo/common/secret.key
 
 echo "Decrypting kubectl config..."
 mkdir -p ~/.kube
-sops -d personal-website-repo/kube_config.enc.yml > ~/.kube/config
+sops -d personal-website-repo-version-bump/kube_config.enc.yml > ~/.kube/config
 chmod 600 ~/.kube/config
 
-cd personal-website-repo
+cd personal-website-repo-version-bump
 echo
 
 helmsman --apply -f dennis-site-DSF.yaml
