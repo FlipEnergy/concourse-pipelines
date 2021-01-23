01f39a14214ae5cc6638397ea295a1916ba10a4e
Dennis Zhang <dennis.zhang.nrg@gmail.com>
2021-01-22 17:48:42 -0800

    use this repo's key

--- personal_website/deploy_to_k8s.sh
+++ personal_website/deploy_to_k8s.sh
@@ -7,7 +7,7 @@ gpg --import concourse-pipelines-repo/common/secret.key
 
 echo "Decrypting kubectl config..."
 mkdir -p ~/.kube
-sops -d personal-website-version-bump/kube_config.enc.yml > ~/.kube/config
+sops -d concourse-pipelines-repo/common/kube_config.enc.yml > ~/.kube/config
 chmod 600 ~/.kube/config
 
 cd personal-website-version-bump
