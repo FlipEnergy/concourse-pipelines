990a70058bbb7504eb4608d6ec51291c8141e0f9
Dennis Zhang <dennis.zhang.nrg@gmail.com>
2021-03-01 14:28:19 -0800

    exit 1 if no secret written

--- common/scripts/get_secret_key.sh
+++ common/scripts/get_secret_key.sh
@@ -10,4 +10,8 @@ mkdir -p secrets
 echo "Getting secret key..."
 bw get item $BW_ITEM | jq -r '.notes' | base64 -d > secrets/secret.key
 
-ls -lh secrets/secret.key
+# exit 1 if file empty
+if ! test -s secrets/secret.key ; then
+  echo 'key not written' 
+  exit 1
+fi
