144ef59a38af0e0e6769e145611cef15ae031bc8
Dennis Zhang <dennis.zhang.nrg@gmail.com>
2021-07-04 23:32:21 -0700

    no banner

--- k8s_homelab/deploy_k8s_homelab.sh
+++ k8s_homelab/deploy_k8s_homelab.sh
@@ -12,4 +12,4 @@ cd k8s-homelab-repo
 echo
 
 kubectl get configmap -n kube-system coredns -o json | sed "s+/etc/resolv.conf+${PIHOLE_SERVER_IP}+g" | kubectl apply -f -
-helmsman -p 3 --apply -f homelab.yaml
+helmsman --no-banner -p 3 --apply -f homelab.yaml
