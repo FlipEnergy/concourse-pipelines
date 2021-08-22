1d74d0ab6fed7e0f0d7ce8e5fec86b02ea154755
Dennis Zhang <dennis.zhang.nrg@gmail.com>
2021-08-21 17:07:53 -0700

    merge scripts

--- k8s-homelab/deploy-k8s-homelab.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -e
-
-concourse-pipelines-repo/common/scripts/import-secret-key.sh
-
-cd k8s-homelab-repo
-echo
-
-kubectl get configmap -n kube-system coredns -o json | sed "s+/etc/resolv.conf+${PIHOLE_SERVER_IP}+g" | kubectl apply -f -
-helmsman --no-banner -p 3 --apply -f homelab.yaml
--- k8s-homelab/deploy-k8s.sh
+++ k8s-homelab/deploy-k8s.sh
@@ -5,4 +5,4 @@ concourse-pipelines-repo/common/scripts/import-secret-key.sh
 cd k8s-homelab-repo
 echo
 
-helmsman --no-banner -p 3 --apply -f oracle.yaml
+helmsman --no-banner -p 3 --apply -f ${DSF_FILE}
