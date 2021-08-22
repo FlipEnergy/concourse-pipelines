fb4455482b29107125577ad3190b0691558b745d
Dennis Zhang <dennis.zhang.nrg@gmail.com>
2021-08-22 13:40:19 -0700

    test site on oracle

--- personal-website/deploy-to-k8s.sh
+++ personal-website/deploy-to-k8s.sh
@@ -7,6 +7,7 @@ export DENNIS_SITE_VERSION=`cat personal-website-repo/.git/short_ref`
 cd personal-website-repo/helm_chart
 echo
 
+kubectl config use-context oracle
 kubectl create ns dennis-site || true
 helm dep update
 helm upgrade -i -n dennis-site dennis-site . --set image.tag=${DENNIS_SITE_VERSION} --wait
