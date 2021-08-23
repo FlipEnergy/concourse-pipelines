ab9197ead7f92433139792689806f1b711849d79
Dennis Zhang <dennis.zhang.nrg@gmail.com>
2021-08-22 19:49:09 -0700

    again

--- personal-website/deploy-to-k8s.sh
+++ personal-website/deploy-to-k8s.sh
@@ -8,11 +8,12 @@ export DENNIS_SITE_VERSION=$(cat "${REPO_DIR}/.git/short_ref")
 cd "${REPO_DIR}/helm_chart"
 
 echo
-echo "Deploying version: ${DENNIS_SITE_VERSION}"
-echo
 
 kubectl config use-context "${KUBE_CONTEXT}"
 kubectl create ns dennis-site || true
 helm dep update
+
+echo
+echo "Deploying version: [${DENNIS_SITE_VERSION}]"
 echo
 helm upgrade -i -n dennis-site dennis-site . --set "image.tag=${DENNIS_SITE_VERSION}" --wait
