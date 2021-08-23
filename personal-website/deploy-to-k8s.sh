c2b02de707a798d951c5526c08a57b9d0045fcd6
Dennis Zhang <dennis.zhang.nrg@gmail.com>
2021-08-22 19:46:25 -0700

    clearer output

--- personal-website/deploy-to-k8s.sh
+++ personal-website/deploy-to-k8s.sh
@@ -3,14 +3,16 @@ set -e
 
 concourse-pipelines-repo/common/scripts/import-secret-key.sh
 
-export DENNIS_SITE_VERSION=`cat "${REPO_DIR}/.git/short_ref"`
+export DENNIS_SITE_VERSION=$(cat "${REPO_DIR}/.git/short_ref")
 
 cd "${REPO_DIR}/helm_chart"
-echo
 
+echo
 echo "Deploying version: ${DENNIS_SITE_VERSION}"
+echo
 
 kubectl config use-context "${KUBE_CONTEXT}"
 kubectl create ns dennis-site || true
 helm dep update
+echo
 helm upgrade -i -n dennis-site dennis-site . --set "image.tag=${DENNIS_SITE_VERSION}" --wait
