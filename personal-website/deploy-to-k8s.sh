acafe972ec009005c441ac0fab3596c322f4d842
Dennis Zhang <dennis.zhang.nrg@gmail.com>
2021-08-22 17:57:29 -0700

    hugo build in concourse

--- personal-website/deploy-to-k8s.sh
+++ personal-website/deploy-to-k8s.sh
@@ -2,12 +2,12 @@
 
 concourse-pipelines-repo/common/scripts/import-secret-key.sh
 
-export DENNIS_SITE_VERSION=`cat personal-website-repo/.git/short_ref`
+export DENNIS_SITE_VERSION=`cat ${REPO_DIR}/.git/short_ref`
 
-cd personal-website-repo/helm_chart
+cd ${REPO_DIR}/helm_chart
 echo
 
-kubectl config use-context default
+kubectl config use-context ${KUBE_CONTEXT}
 kubectl create ns dennis-site || true
 helm dep update
 helm upgrade -i -n dennis-site dennis-site . --set image.tag=${DENNIS_SITE_VERSION} --wait
