2b9a991489d4986472e56e67e3b29af0eb190e8c
Dennis Zhang <dennis.zhang.nrg@gmail.com>
2022-04-05 23:06:48 -0700

    remove cert apply

--- k8s-homelab/deploy-k8s.sh
+++ k8s-homelab/deploy-k8s.sh
@@ -6,5 +6,4 @@ concourse-pipelines-repo/common/scripts/import-secret-key.sh
 cd "${REPO_DIR}"
 echo
 
-sops -d ingress-nginx/secret.default-ssl-certs.yaml | kubectl --context "$KUBE_CONTEXT" -n default apply -f -
 helmsman --no-banner -p 3 --apply -f "${KUBE_CONTEXT}.yaml"
