3558cc4dcf93c83fe97d9bc9eb8f5651f708c274
Dennis Zhang <dennis.zhang.nrg@gmail.com>
2022-05-18 11:19:25 -0700

    login in retry

--- common/scripts/get-secret-key.sh
+++ common/scripts/get-secret-key.sh
@@ -3,14 +3,14 @@
 echo "Logging in..."
 bw config server "$BW_SERVER"
 
-export BW_SESSION=`bw login "$BW_USERNAME" "$BW_PASSWORD" --raw`
-
 mkdir -p secrets
 
 echo "Getting secret key..."
 
 retries=5
 while [ "$retries" -gt 0 ]; do
+  export BW_SESSION=`bw login "$BW_USERNAME" "$BW_PASSWORD" --raw`
+
   bw get item "$BW_ITEM" | jq -r '.notes' | base64 -d > secrets/secret.key
 
   if test -s secrets/secret.key ; then
