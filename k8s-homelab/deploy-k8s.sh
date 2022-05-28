d6ef5bf5c04621c5bdb04340e7e378aa4c1e42ba
Dennis Zhang <dennis.zhang.nrg@gmail.com>
2022-05-27 17:26:50 -0700

    retry on failure

--- k8s-homelab/deploy-k8s.sh
+++ k8s-homelab/deploy-k8s.sh
@@ -6,4 +6,13 @@ concourse-pipelines-repo/common/scripts/import-secret-key.sh
 cd "${REPO_DIR}"
 echo
 
-helmsman --no-banner -p 3 "$ACTION" -f "${KUBE_CONTEXT}.yaml"
+retries=3
+while [ "$retries" -gt 0 ]; do
+  if helmsman --no-banner -p 3 "$ACTION" -f "${KUBE_CONTEXT}.yaml"; then
+    exit 0
+  fi
+  echo
+  echo 'Retrying...'
+  retries=$((retries - 1))
+done
+exit 1
