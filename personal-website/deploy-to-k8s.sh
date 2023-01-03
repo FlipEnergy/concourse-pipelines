c6474cfbb8c3ecac3ae6674b6ec77a40882f0c3a
Dennis Zhang <dennis.zhang.nrg@gmail.com>
2023-01-03 18:30:06 +0000

    update personal site deploy

--- personal-website/deploy-to-k8s.sh
+++ personal-website/deploy-to-k8s.sh
@@ -10,9 +10,9 @@ cd "${REPO_DIR}/helm_chart"
 echo
 
 kubectl config use-context "${KUBE_CONTEXT}"
-helm dep update
+helm repo add bjw-s https://bjw-s.github.io/helm-charts
 
 echo
 echo "Deploying version: [${DENNIS_SITE_VERSION}]"
 echo
-helm upgrade -i -n default dennis-site . --set-string "image.tag=${DENNIS_SITE_VERSION}" --wait
+helm upgrade -i -n default dennis-site bjw-s/app-template --version 1.2.0 -f values.yaml --set-string "image.tag=${DENNIS_SITE_VERSION}" --wait
