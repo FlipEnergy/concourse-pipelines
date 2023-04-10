a953235a1e0aa36e59c645e61defb10a94134a80
Dennis Zhang <dennis.zhang.nrg@gmail.com>
2023-04-10 19:57:27 +0000

    test 9

--- common/scripts/get-bw-item.sh
+++ common/scripts/get-bw-item.sh
@@ -7,30 +7,24 @@ mkdir -p secrets
 
 echo "Getting bitwarden secret..."
 
+echo "Making session"
+export BW_SESSION=$(bw login "$BW_USERNAME" "$BW_PASSWORD" --raw)
 
 for field in $BW_FIELDS; do
-  retries=10
-  while [ "$retries" -gt 0 ]; do
-    if [ -z "$BW_SESSION" ]; then
-      echo "Making session"
-      export BW_SESSION=$(bw login "$BW_USERNAME" "$BW_PASSWORD" --raw)
-    fi
-
+  for i in {1..3}; do
     echo "Getting ${field}..."
-    bw get item "$BW_ITEM" | jq -r ".$field" > secrets/"$field".txt
+    bw get item "$BW_ITEM" | jq -r ".${field}" > "secrets/${field}.txt"
 
-    if test -s secrets/"$field".txt ; then
+    if test -s "secrets/$field.txt" ; then
       echo 'Got the goods'
       break
     fi
 
     echo 'file not written, retrying'
-    retries=$((retries - 1))
-    sleep $((10 - retries))
+    sleep 1
+    if [ $i -eq 3 ]; then
+      echo 'Failed to get secret'
+      exit 1
+    fi
   done
 done
-
-
-
-
-exit 1
